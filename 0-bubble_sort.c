aec0f13caa5151313d8a1119027d8275d7848cd5
root <6328@holbertonstudents.com>
2023-05-18 09:46:35 -0700

    fixed task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function that sorts an array of integers.
+ *
+ * @array: Elements array
+ *
+ * @size: Array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, swap;
+
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = swap;
+				print_array(array, size);
+			}
+        }
+	}
+}
