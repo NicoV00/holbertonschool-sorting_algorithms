e896b2b48ec9110a20b06f0777e453ac5570cc7a
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-05-21 12:52:22 -0700

    fix errors

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,29 +8,26 @@
 */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_index, temp;
+    int i, j, min_index, temp;
 
-    for (i = 0; i < size - 1; i++)
+    for (i = 0; i < size; i++)
     {
+        min_index = 0;
         min_index = i;
 
         for (j = i + 1; j < size; j++)
         {
-            if (array[j] < array[min_index])
-                min_index = j;
+            if (array[min_index] > array[j])
+            {
+                temp = array[j];
+                array[j] = array[min_index];
+                array[min_index] = temp;
+                print_array(array, size);
+            }
         }
-
-        if (min_index != i)
-        {
-            temp = array[i];
-            array[i] = array[min_index];
-            array[min_index] = temp;
-        }
-
-        print_array(array, size);
+        
     }
 }
-
 
 
 
