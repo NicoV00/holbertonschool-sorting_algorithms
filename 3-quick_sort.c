571fc8ca4c6fb54cdd331a625a61a5d1b3fbafac
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-05-23 09:09:17 -0700

    12

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,63 +1,76 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
-int partition(int *array, int low, int high)
+/**
+ * partition - Lomuto partition scheme
+ * @array: array to be sorted
+ * @low: starting index
+ * @high: ending index
+ * @size: size of the array
+ *
+ * Return: index of the pivot
+ */
+int partition(int *array, int low, int high, size_t size)
 {
-	int pivot = array[high];
-	int x, i = low - 1, j;
+    int pivot = array[high];
+    int i = low - 1;
+    int j, temp;
 
-	for (j = low; j <= high - 1; j++)
-	{
-		if (array[j] <= pivot)
-		{
-			i++;
-			x = array[i];
-			array[i] = array[j];
-			array[j] = x;
-		}
-	}
-	x = array[i + 1];
-	array[i + 1] = array[high];
-	array[high] = x;
-	return (i + 1);
+    for (j = low; j <= high - 1; j++)
+    {
+        if (array[j] <= pivot)
+        {
+            i++;
+            if (i != j)
+            {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                print_array(array, size);
+            }
+        }
+    }
+
+    if (array[i + 1] != array[high])
+    {
+        temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+        print_array(array, size);
+    }
+
+    return (i + 1);
 }
 
+/**
+ * quick_sort_helper - recursive function to perform quick sort
+ * @array: array to be sorted
+ * @low: starting index
+ * @high: ending index
+ * @size: size of the array
+ */
+void quick_sort_helper(int *array, int low, int high, size_t size)
+{
+    int pivot;
+
+    if (low < high)
+    {
+        pivot = partition(array, low, high, size);
+
+        quick_sort_helper(array, low, pivot - 1, size);
+        quick_sort_helper(array, pivot + 1, high, size);
+    }
+}
+
+/**
+ * quick_sort - sorts an array of integers in ascending order using Quick sort
+ * @array: array to be sorted
+ * @size: size of the array
+ */
 void quick_sort(int *array, size_t size)
 {
-	int *stack = NULL;
-	int top = -1;
+    if (array == NULL || size < 2)
+        return;
 
-	if (size <= 1)
-		return;
-
-	stack = malloc(size * sizeof(int));
-	if (stack == NULL)
-	{
-		return;
-	}
-
-	stack[++top] = 0;
-	stack[++top] = size - 1;
-
-	while (top >= 0)
-	{
-		int high = stack[top--];
-		int low = stack[top--];
-		int pivot_index = partition(array, low, high);
-
-		if (pivot_index - 1 > low)
-		{
-			stack[++top] = low;
-			stack[++top] = pivot_index - 1;
-		}
-		if (pivot_index + 1 < high)
-		{
-			stack[++top] = pivot_index + 1;
-			stack[++top] = high;
-		}
-		print_array(array, size);
-	}
-
-	free(stack);
+    quick_sort_helper(array, 0, size - 1, size);
+	print_array(array, size);
 }
