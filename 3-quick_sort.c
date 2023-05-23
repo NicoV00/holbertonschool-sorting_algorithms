f4f31af242868995045e20ca4f40d85922b4829b
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-05-23 08:18:04 -0700

    2

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -14,7 +14,7 @@ int partition(int *array, int low, int high)
 	int pivot = array[high];
 	int x, i = low - 1;
 
-	for (int j = low; j <= high - 1; j++)
+	for (j = low; j <= high - 1; j++)
 	{
 		if (array[j] <= pivot)
 		{
