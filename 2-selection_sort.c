72202f4b0a37d03850310b8e5a3a8361debe5380
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-05-23 12:20:56 -0700

    qw

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,31 +1,28 @@
 #include "sort.h"
-
 /**
- * selection_sort- use the select_sort
- * @array: array of ints 
- * @size: this is the size of the array.
- * 
+* selection_sort- use the select_sort
+* @array: array of ints
+* @size: this is the size of the array.
 */
 void selection_sort(int *array, size_t size)
 {
-    int temp;
-    size_t i, j, min_idx;
+	int temp;
+	size_t i, j, min_idx;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
-
-        if (min_idx != i)
-        {
-            temp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = temp;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+			min_idx = j;
+		}
+		if (min_idx != i)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
+	}
 }
